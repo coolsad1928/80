62b1d067d6c995fba5a67e5e8a584586c5e118de
coolsad1928 <102434304+coolsad1928@users.noreply.github.com>
2022-04-09 14:44:49 +0300

    Add files via upload

---
+++ github/User Folder/noticeboard.php
@@ -0,0 +1,103 @@
+<?php
+include("design.php");
+$conn=mysqli_connect("localhost","root","","admissionhelperadmin");
+if(isset($_POST['search'])) {
+  $searchkey = $_POST['search'];
+  $sql = "SELECT* FROM noticeboard WHERE uname LIKE '%$searchkey%'";
+}else{
+  $sql = "SELECT * FROM noticeboard";
+  $searchkey = "";
+}
+
+
+
+?>
+
+<br>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style> 
+input[type=text] {
+  width: 130px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background-color: white;
+  background-image: url('searchicon.png');
+  background-position: 10px 10px; 
+  background-repeat: no-repeat;
+  padding: 12px 20px 12px 40px;
+  -webkit-transition: width 0.4s ease-in-out;
+  transition: width 0.4s ease-in-out;
+}
+
+input[type=text]:focus {
+  width: 100%;
+}
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+
+
+
+
+<form method="POST" action="search.php">
+  <input type="text" name="search" placeholder="Search By University Name..." value="<?php echo $searchkey; ?>"><br>
+  <button type="submit" name="submit">Search Now</button>
+</form>
+
+
+<table>
+  <thead>
+  <tr>
+    <th>Name </th>
+    <th>Subject </th>
+    <th>Publish Date </th>
+    <th>Link </th>
+    
+  </tr>
+  </thead>
+  <tbody>
+    <?php
+    $sql="SELECT * from noticeboard ";
+    $result=$conn ->query($sql);
+    if ($result -> num_rows >0) {
+      while ($row=$result ->fetch_assoc()) {
+        # code...
+        echo "
+      <tr>
+      <td>".$row['uname']."</td>
+      <td>".$row['Sbuject']."</td>
+      <td>".$row['date']."</td>
+      <td>".$row['link']."</td>
+
+    </tr>";
+      }
+    }
+    
+  ?>
+  </tbody>
+</table><br>
+
+
+    </body>
+</html>
---
+++ github/admin/index.php
@@ -0,0 +1,153 @@
+<?php
+include("design.php");
+?>
+
+<!DOCTYPE html>
+<html>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
+.w3-bar-block .w3-bar-item {padding:20px}
+</style>
+<body>
+
+
+
+
+  
+
+<div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px">
+<center>
+  
+  <div class="w3-row-padding w3-padding-16 w3-center" id="food">
+    <div class="w3-quarter">
+      <img src=" img/diu.png" alt="Sandwich" style="background position=cover; width:50%;">
+      <h3>специальности университета ргсу</h3>
+      <p> 
+      факультет информационных технологий<br>
+▶програмирование<br>
+▶системное администрирование<br>
+▶разработка<br>
+▶сети<br>
+      </p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/BUETt.jpg" alt="Steak" style="width:90%">
+      <h3>Факультет информационной безопасности</h3>
+      <p> 
+➽Противодействие вирусным угрозам<br>
+➽Методики противодействия хакерским угрозам<br>
+➽Защита сетей<br>
+      </p>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/date.jpg" alt="Cherries" style="width:100%">
+      <h3>Экономика</h3>
+      <p>
+        
+
+      </p>
+      
+    </div>
+    <div class="w3-quarter">
+      <img src="img/admission4.jpg" alt="Pasta and Wine" style="width:100%">
+      <h3>>Юр.факультет</h3>
+    </div>
+  </div>
+  
+  
+  <div class="w3-row-padding w3-padding-16 w3-center">
+    <div class="w3-quarter">
+      <img src="img/buet.jpg" alt="Popsicle" style="width:100%">
+      <h3>Образование начальных классов</h3>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/nsu.jpg" alt="Salmon" style="width:30%">
+      <h3>Биохимический</h3>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/uap.png" alt="Sandwich" style="width:30%">
+      <h3>История</h3>
+    </div>
+    <div class="w3-quarter">
+      <img src="img/7college.jpg" alt="Croissant" style="width:100%">
+    
+    </div>
+  </div>
+
+  
+  <div class="w3-center w3-padding-32">
+    <div class="w3-bar">
+      <a href="#" class="w3-bar-item w3-button w3-hover-black">«</a>
+      <a href="#" class="w3-bar-item w3-black w3-button">1</a>
+      <a href="#" class="w3-bar-item w3-button w3-hover-black">2</a>
+      <a href="#" class="w3-bar-item w3-button w3-hover-black">3</a>
+      <a href="#" class="w3-bar-item w3-button w3-hover-black">4</a>
+      <a href="#" class="w3-bar-item w3-button w3-hover-black">»</a>
+    </div>
+  </div>
+  
+  <hr id="about">
+
+  
+  <div class="w3-container w3-padding-32 w3-center">  
+    
+    <img src="img/admission2.jpg" alt="Me" class="w3-image" style="display:block;margin:auto" width="1000" height="533">
+    <div class="w3-padding-32">
+      <h4><b> </b></h4>
+      <h6><i>Колледж </i></h6>
+    </div>
+  </div>
+  <hr>
+  
+  
+  <footer class="w3-row-padding w3-padding-32">
+   
+    <div class="w3-third">
+      <h3>For Any Querry</h3>
+      <ul class="w3-ul w3-hoverable">
+        <li class="w3-padding-16">
+          <img src="img/loc.png" class="w3-left w3-margin-right" style="width:50px">
+          <span class="w3-large">Office</span><br>
+          <span>Sukrabad, Dhanmondi32, Dhaka1209</span>
+        </li>
+        <li class="w3-padding-16">
+          <img src="img/mail.jpg" class="w3-left w3-margin-right" style="width:50px">
+          <span class="w3-large">Mail@</span><br>
+          <span>sowvick35-1562@diu.edu.bd</span>
+        </li> 
+        <li class="w3-padding-16">
+          <img src="img/fb.jpg" class="w3-left w3-margin-right" style="width:50px">
+          <span class="w3-large">Facebook</span><br>
+          <span>http//:facebook.admisssionhelper.com</span>
+        </li> 
+      </ul>
+    </div>
+
+    <div class="w3-third w3-serif">
+     
+    </div>
+  </footer>
+</center>
+
+</div>
+
+<script>
+
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+     </script>
+
+</body>
+</html>
+
+
